ed6e8b9654d1f4acd3f493595a599d0d873e4f80
prynnekey <prynnekey@gmail.com>
2023-02-19 18:38:30 +0800

    Feat mongoDB

---
+++ models/init.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Mongo = initMongo()
+
+func initMongo() *mongo.Database {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client.Database("im")
+}
