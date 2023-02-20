5fc6288b44b103b995c119b2ea7a23fa0b19d124
prynnekey <prynnekey@gmail.com>
2023-02-20 19:58:17 +0800

    Feat TestFind for user_room

--- test/mongo_test.go
+++ test/mongo_test.go
@@ -28,3 +28,35 @@ func TestFindOne(t *testing.T) {
 
 	fmt.Printf("ub: %v\n", ub)
 }
+
+// 通过房间号获取用户列表
+func TestFind(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	db := client.Database("im")
+	cursor, err := db.Collection("user_room").Find(context.Background(), bson.D{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 存储房间结果
+	urs := make([]*models.UserRoom, 0)
+	for cursor.Next(context.Background()) {
+		ur := &models.UserRoom{}
+		err := cursor.Decode(ur)
+		if err != nil {
+			t.Fatal(err)
+		}
+		urs = append(urs, ur)
+	}
+
+	// 遍历打印
+	for _, ur := range urs {
+		fmt.Printf("user_room: %v\n", ur)
+	}
+}
