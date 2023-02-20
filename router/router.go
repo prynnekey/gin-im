6adee7fc1bdb45b4082113b54ca7e4de69609568
prynnekey <prynnekey@gmail.com>
2023-02-20 12:48:02 +0800

    Feat authorization router

--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/prynnekey/gin-im/middleware"
 	"github.com/prynnekey/gin-im/service"
 )
 
@@ -12,6 +13,13 @@ func Init() *gin.Engine {
 	r.POST("/login", service.Login())
 	// 用户注册
 	r.POST("/register", service.Register())
+
+	// 需要登录才能访问的接口
+	auth := r.Group("/api", middleware.AuthHandler())
+	{
+		// 获取用户详情
+		auth.GET("/user/detail", service.UserDetail())
+	}
 
 	return r
 }
