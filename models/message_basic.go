135a8c1b89637e690017fa09b64e73f7ce809945
prynnekey <prynnekey@gmail.com>
2023-02-20 22:23:34 +0800

    Feat get history by romm identity

--- models/message_basic.go
+++ models/message_basic.go
@@ -1,6 +1,11 @@
 package models
 
-import "context"
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 type MessageBasic struct {
 	Identity     string `bson:"identity"`
@@ -18,4 +23,32 @@ func (MessageBasic) CollectionName() string {
 func InsertOneMessageBasic(messageBasic *MessageBasic) error {
 	_, err := Mongo.Collection(MessageBasic{}.CollectionName()).InsertOne(context.Background(), messageBasic)
 	return err
+}
+
+// 根据房间identity获取聊天记录
+func GetMessageBasicByRootIdentity(rootIdentity string, pageIndex, pageSize int64) ([]*MessageBasic, error) {
+	var messageBasics []*MessageBasic
+
+	offset := (pageIndex - 1) * pageSize
+
+	cursor, err := Mongo.Collection(MessageBasic{}.CollectionName()).
+		Find(context.Background(), bson.M{"root_identity": rootIdentity},
+			// 分页查询
+			&options.FindOptions{
+				Limit: &pageSize,               // Limit
+				Skip:  &offset,                 // Offset
+				Sort:  bson.M{"create_at": -1}, // Sort
+			})
+
+	if err != nil {
+		return nil, err
+	}
+
+	for cursor.Next(context.Background()) {
+		mb := &MessageBasic{}
+		cursor.Decode(mb)
+		messageBasics = append(messageBasics, mb)
+	}
+
+	return messageBasics, err
 }
