c20c1c85be7835bbabcbe8f20aefda61124d7f52
prynnekey <prynnekey@gmail.com>
2023-02-22 18:13:36 +0800

    Feat getter

--- models/room_basic.go
+++ models/room_basic.go
@@ -17,8 +17,8 @@ type RoomBasic struct {
 }
 
 type RoomBasicSimple struct {
-	Name string `bson:"name"`
-	Info string `bson:"info"`
+	Name string `json:"name"`
+	Info string `json:"info"`
 }
 
 func (RoomBasic) CollectionName() string {
@@ -43,6 +43,14 @@ func GetRoomBasicByRoomNumber(number string) (*RoomBasic, error) {
 	rb := &RoomBasic{}
 	err := Mongo.Collection(RoomBasic{}.CollectionName()).
 		FindOne(context.Background(), bson.D{{Key: "number", Value: number}}).Decode(rb)
+
+	return rb, err
+}
+
+func GetRoomBasicByRoomIdentity(identities string) (*RoomBasic, error) {
+	rb := &RoomBasic{}
+	err := Mongo.Collection(RoomBasic{}.CollectionName()).
+		FindOne(context.Background(), bson.D{{Key: "identity", Value: identities}}).Decode(rb)
 
 	return rb, err
 }
